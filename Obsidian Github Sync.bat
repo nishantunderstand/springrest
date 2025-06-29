28b07f92601e93d074ecc8f7259e7c51027dbfc4
NishantRaj <nishant.read@gmail.com>
2025-06-29 20:36:01 +0530

    LenovoBat 29-Jun-25 20:36:00.97

---
+++ Obsidian Github Sync.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo springrest-Begin
+	set "repoLocation=C:\Users\User\eclipse-workspace\springrest"
+	set "gitPath=C:\Program Files\Git\cmd\git.exe"
+	cd /d "%repoLocation%"
+	if not exist "%repoLocation%\.git" (
+		"%gitPath%" init
+	)
+	"%gitPath%" pull
+	"%gitPath%" add .
+		set commitMessage=LenovoBat %date% %time%
+	"%gitPath%" commit -m "%commitMessage%"
+	"%gitPath%" push
+echo springrest-End
+
+
+
+
+
+
+
+
+echo This window will close in 15 seconds...
+timeout /t 15 >nul
+exit
