cb1386739cca721625b35a4912ccfbc0245a0b8d
Nishant Raj <nishant.read@gmail.com>
2026-05-09 23:29:24 +0530

    LenovoBat 09-May-26 23:29:23.96

---
+++ src/main/java/restfulwebservices/_test/A.java
@@ -0,0 +1,10 @@
+package restfulwebservices._test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>();
+    }
+}
